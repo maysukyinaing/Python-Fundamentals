02a377b52a16621d4bfc20c5a473a42db99271e2
MaySuKyiNaing <maysukyinaing@gmail.com>
2023-02-05 14:38:13 +0630

    index, In & Not in

--- 1_array.py
+++ 1_array.py
@@ -19,5 +19,23 @@ def test():
 
     print(A[3:-3]) #[]
 
+    #The len() function returns the number of items in an object.
+    lenFunction=['cat','dog','tiger']
+    print(len(lenFunction))  # 3
+
+    #list Concatenation
+    listConcat1=[1, 2, 3] + ['A', 'B', 'C']
+    print(listConcat1)
+    listConcat2 = ['A', 'B', 'C'] * 2
+    print(listConcat2)
+
+    #In & Not in Operator
+    print('he' in ['hello', 'hi', 'nice to meet you'])
+
+    #Index value in list
+    list = ['hello', 'hi', 'nice to meet you']
+    print(list.index('hi'))
+
+
 if __name__ == '__main__':
     test()
