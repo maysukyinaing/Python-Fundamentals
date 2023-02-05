6ce86d3b4d02e4550667a028002255bc77a05c00
MaySuKyiNaing <maysukyinaing@gmail.com>
2023-02-05 14:12:06 +0630

    array

---
+++ 1_array.py
@@ -0,0 +1,21 @@
+def test():
+        #  -4     -3  -2  -1   <------
+    A = ['apple', 3, 4.1, -2]
+        #  0      1   2    3   ------>
+    print("normal indexing start at 0 -------------- ")
+    print(A[2]) #picking item at index 2
+
+    print("negate index ----------")
+    print(A[-2])
+
+    print("slicing-------- (include, exclude)")
+    print(A[1:3])   #[3, 4.1]
+    print(A[2:3])   #[4.1]
+    print(A[3:3])   #[]
+
+    print(A[-1:3])  #[]
+    print(A[-2:3])  #[4.1]
+    print(A[-3:3])  #[3, 4.1]
+
+if __name__ == '__main__':
+    test()
